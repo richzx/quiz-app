7997df48874c917fd423a9550020065e3484d8cf
richz <richarmiguelrodriguez@gmail.com>
2024-03-11 00:25:41 -0500

    data_config

---
+++ src/data/data.ts
@@ -0,0 +1,79 @@
+export const data = [
+  {
+    id: 0,
+    title: "How many colors are there in a rainbow ?",
+    options: [
+      { title: "Five", id: 1, questionId: 0 },
+      { title: "Eight", id: 2, questionId: 0 },
+      { title: "Seven", id: 3, questionId: 0 },
+      { title: "Six", id: 4, questionId: 0 },
+    ],
+    correctOption: { id: 3 },
+  },
+  {
+    id: 1,
+    title: "Where is the Eiffel Tower ?",
+    options: [
+      { title: "Italy", id: 1, questionId: 1 },
+      { title: "Paris", id: 2, questionId: 1 },
+      { title: "New York", id: 3, questionId: 1 },
+      { title: "Tehran", id: 4, questionId: 1 },
+    ],
+    correctOption: { id: 2 },
+  },
+  {
+    id: 2,
+    title: "In which country was golf first played ?",
+    options: [
+      { title: "Scotland", id: 1, questionId: 2 },
+      { title: "Canada", id: 2, questionId: 2 },
+      { title: "Germany", id: 3, questionId: 2 },
+      { title: "Korea", id: 4, questionId: 2 },
+    ],
+    correctOption: { id: 1 },
+  },
+  {
+    id: 3,
+    title: "What are primary colors ?",
+    options: [
+      { title: "Black , White", id: 1, questionId: 3 },
+      { title: "Green , Red , Yellow", id: 2, questionId: 3 },
+      { title: "Blue , Red", id: 3, questionId: 3 },
+      { title: "Red , Blue , Yellow", id: 4, questionId: 3 },
+    ],
+    correctOption: { id: 4 },
+  },
+  {
+    id: 4,
+    title: "Wich is the largest member of the cat family ?",
+    options: [
+      { title: "Lion", id: 1, questionId: 4 },
+      { title: "Tiger", id: 2, questionId: 4 },
+      { title: "Street Cat", id: 3, questionId: 4 },
+      { title: "None of above", id: 4, questionId: 4 },
+    ],
+    correctOption: { id: 2 },
+  },
+  {
+    id: 5,
+    title: "How many legs does a spider have ?",
+    options: [
+      { title: "Five", id: 1, questionId: 5 },
+      { title: "Eight", id: 2, questionId: 5 },
+      { title: "Seven", id: 3, questionId: 5 },
+      { title: "Six", id: 4, questionId: 5 },
+    ],
+    correctOption: { id: 4 },
+  },
+  {
+    id: 6,
+    title: "What are Google Chrome and Firefox ?",
+    options: [
+      { title: "Operation System", id: 1, questionId: 6 },
+      { title: "Web Browsers", id: 2, questionId: 6 },
+      { title: "Media Players", id: 3, questionId: 6 },
+      { title: "Photo Editors", id: 4, questionId: 6 },
+    ],
+    correctOption: { id: 2 },
+  },
+];
